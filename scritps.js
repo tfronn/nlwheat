150dae08226170bc9c0111b6c26ffafa8cc9676d
Tfronn <keeelvincamilo@hotmail.com>
2021-10-22 10:23:13 -0300

    aplicação finalizada

--- scritps.js
+++ scritps.js
@@ -15,3 +15,17 @@ function changeSocialMediaLinks() {
 }
 
 changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      username.textContent = data.name
+      bio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
